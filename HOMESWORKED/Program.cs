a7dec456a1cd8e59f887254b08c664e4619fc1de
BTFspec <btfack@gmail.com>
2024-02-28 21:47:10 +0600

    ...

--- HOMESWORKED/Program.cs
+++ HOMESWORKED/Program.cs
@@ -4,14 +4,14 @@
 
 // 1 Созданный массив готовый без функции
 
-using System.ComponentModel.DataAnnotations;
+// using System.ComponentModel.DataAnnotations;
 
-int[,] matr = new int[,] {
-    {1, 2, 3},
-    {7, 8, 2},
-    {1, 1, 0},
-    {9, 10, 11}
-};
+// int[,] matr = new int[,] {
+//     {1, 2, 3},
+//     {7, 8, 2},
+//     {1, 1, 0},
+//     {9, 10, 11}
+// };
 
 // 2 ФУНКЦИЯ ПЕЧАТЬ МАТРИЦЫ ДВУХМЕРНОГО МАССИВА
 
@@ -29,44 +29,44 @@ int[,] matr = new int[,] {
 
 // 3 ФУНКЦИЯ которая будет находить cуммы строк
 
-int[] GetArrayWithMeans(int[,] array) 
-{                                                              
-    int[] sum = new int[array.GetLength(0)];
-      for (int i = 0; i < array.GetLength(0); i++)
-      {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-          sum[i] += array[i,j];
-        }
-      }
-      return sum;
-}
+// int[] GetArrayWithMeans(int[,] array) 
+// {                                                              
+//     int[] sum = new int[array.GetLength(0)];
+//       for (int i = 0; i < array.GetLength(0); i++)
+//       {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//           sum[i] += array[i,j];
+//         }
+//       }
+//       return sum;
+// }
 
-// 4 ФУНКЦИЯ КОТОРАЯ БУДЕТ НАХОДИТЬ ИНДЕКС СТРОКИ С МИНИМАЛЬНЫМИ ЗНАЧЕНИЯМИ
-int MinIndex(int[] array)
-{
-     int minI = 0;
-      for (int i =0; i < array.Length; i++)
-      {
-        if(array[minI] >array[i])
-        {
-          minI = i;
-        }
-      }
-    //   Console.WriteLine();
-      return minI;
-}
+// // 4 ФУНКЦИЯ КОТОРАЯ БУДЕТ НАХОДИТЬ ИНДЕКС СТРОКИ С МИНИМАЛЬНЫМИ ЗНАЧЕНИЯМИ
+// int MinIndex(int[] array)
+// {
+//      int minI = 0;
+//       for (int i =0; i < array.Length; i++)
+//       {
+//         if(array[minI] >array[i])
+//         {
+//           minI = i;
+//         }
+//       }
+//     //   Console.WriteLine();
+//       return minI;
+// }
 
-                                                                  // 2 ФУНЦИЯ ВЫЗОВ
+//                                                                   // 2 ФУНЦИЯ ВЫЗОВ
 
-    //PrintMatrix(matr); // это вызвали функцию печать массива; мы ее выше сделали так мы ее вызываем;
+//     //PrintMatrix(matr); // это вызвали функцию печать массива; мы ее выше сделали так мы ее вызываем;
 
 
-                                                                         // 3 ФУНЦИЯ ВЫЗОВ
+//                                                                          // 3 ФУНЦИЯ ВЫЗОВ
 
- int[] res = GetArrayWithMeans(matr);
-                                                                              // 4 ФУНЦИЯ ВЫЗОВ                                                              
- Console.WriteLine($"{MinIndex(res)}"); 
+//  int[] res = GetArrayWithMeans(matr);
+//                                                                               // 4 ФУНЦИЯ ВЫЗОВ                                                              
+//  Console.WriteLine($"{MinIndex(res)}"); 
 
 
 
@@ -148,3 +148,37 @@ int MinIndex(int[] array)
 // PrintArray(array);
 // int row = SumRowArray(array);
 // Console.WriteLine($"Наименьшая сумма в {row} строке.");
+
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет: "Позиция по рядам выходит за пределы массива"
+// или
+// "Позиция по колонкам выходит за пределы массива"
+
+// Позиции в массиве считать от единицы.
+
+// В коде, начальные условия:
+
+int[,] numbers = new int[,] {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12}
+};
+
+int x = 2; // колонна
+int y = 2; // ряд строка
+//Выводится: 6;
+
+if (y > numbers.GetLength(0))
+{
+    Console.WriteLine("Позиция по рядам выходит за пределы массива");
+}
+if (x > numbers.GetLength(1))
+{
+    Console.WriteLine("Позиция по колонкам выходит за пределы массива");
+}
+else
+{
+//   int sum = 0;
+// sum = numbers;
+    Console.WriteLine($" {numbers[y,x]}");   //($" {numbers[y-1,x-1]}");
+}
+
--- Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+721578b4419c6f74f853698f45925006cd822b3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4733da04969d2de684acbba4b1d0649a7d435f80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

4733da04969d2de684acbba4b1d0649a7d435f80
BTFspec <btfack@gmail.com>
2024-02-28 18:34:48 +0600

    ///////.>

--- HOMESWORKED/Program.cs
+++ HOMESWORKED/Program.cs
@@ -1,1 +1,150 @@
-﻿
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Начальные условия:
+
+// 1 Созданный массив готовый без функции
+
+using System.ComponentModel.DataAnnotations;
+
+int[,] matr = new int[,] {
+    {1, 2, 3},
+    {7, 8, 2},
+    {1, 1, 0},
+    {9, 10, 11}
+};
+
+// 2 ФУНКЦИЯ ПЕЧАТЬ МАТРИЦЫ ДВУХМЕРНОГО МАССИВА
+
+// void PrintMatrix(int[,] matr)  // ФУНКЦИЯ ВЫВОДА МАССИВА НА ЭКРАН КОТОРЫЙ МЫ СДЕЛАЛИ ВЫШЕ                       Цикл в цикле получается
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++) // стр  //попали в строчку обошли все столбцы; потом след строчка также
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++) // столбцы
+//         {
+//             Console.Write($"{matr[i, j]}\t"); // "\t" = 4 пробела 
+//         } // Закончили обход 0 строки, идем в 1
+//         Console.WriteLine(); // Перенос на новую строчку \\ это чтобы каждая строка массива были раздельно а не на одной строке
+//     }
+// }
+
+// 3 ФУНКЦИЯ которая будет находить cуммы строк
+
+int[] GetArrayWithMeans(int[,] array) 
+{                                                              
+    int[] sum = new int[array.GetLength(0)];
+      for (int i = 0; i < array.GetLength(0); i++)
+      {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+          sum[i] += array[i,j];
+        }
+      }
+      return sum;
+}
+
+// 4 ФУНКЦИЯ КОТОРАЯ БУДЕТ НАХОДИТЬ ИНДЕКС СТРОКИ С МИНИМАЛЬНЫМИ ЗНАЧЕНИЯМИ
+int MinIndex(int[] array)
+{
+     int minI = 0;
+      for (int i =0; i < array.Length; i++)
+      {
+        if(array[minI] >array[i])
+        {
+          minI = i;
+        }
+      }
+    //   Console.WriteLine();
+      return minI;
+}
+
+                                                                  // 2 ФУНЦИЯ ВЫЗОВ
+
+    //PrintMatrix(matr); // это вызвали функцию печать массива; мы ее выше сделали так мы ее вызываем;
+
+
+                                                                         // 3 ФУНЦИЯ ВЫЗОВ
+
+ int[] res = GetArrayWithMeans(matr);
+                                                                              // 4 ФУНЦИЯ ВЫЗОВ                                                              
+ Console.WriteLine($"{MinIndex(res)}"); 
+
+
+
+
+// // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку
+// // с наименьшей суммой элементов.
+// // Например, задан массив:
+// // 1 4 7 2
+// // 5 9 2 3
+// // 8 4 2 4
+// // 5 2 6 7
+// // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей
+// // суммой элементов: 1 строка
+
+// int Prompt(string message)
+// {
+//   Console.Write(message);
+//   int result = Convert.ToInt32(Console.ReadLine());
+//   return result;
+// }
+
+// int[,] CreateArray(int m, int n)
+// {
+//   int[,] array = new int[m, n];
+//   return array;
+// }
+
+// void PrintArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write($"{array[i, j]} ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
+// int[,] FillArrayRandom(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       array[i, j] = new Random().Next(1, 10);
+//     }
+//   }
+//   return array;
+// }
+
+// int SumRowArray(int[,] array)
+// {
+//   int[] sumArr = new int[array.GetLength(0)];
+//   Console.WriteLine($"Сумма по строке:");
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       sumArr[i] += array[i, j];
+//     }
+//     Console.WriteLine($"{i + 1} -> {sumArr[i]}");
+//   }
+//   int minSum = sumArr[0];
+//   int minRow = 1;
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     if (sumArr[i] < minSum)
+//     {
+//       minSum = sumArr[i];
+//       minRow = i + 1;
+//     }
+//   }
+//   return minRow;
+// }
+
+// int[,] array = CreateArray(Prompt("Введите количество строк: "), Prompt("Введите количество столбцов: "));
+// array = FillArrayRandom(array);
+// PrintArray(array);
+// int row = SumRowArray(array);
+// Console.WriteLine($"Наименьшая сумма в {row} строке.");
---
+++ seminar_6/Program.cs
@@ -0,0 +1,64 @@
+﻿//СЛОЖЕНИЕ СТРОК КОНКАТЕНАЦИЯ 
+//"ПРИВЕТ" + "МИР" = "ПРИВЕТМИР"
+// "ПРИВЕТ" СТРОКА СОСТОИТ ИЗ НЕСКОЛЬКИХ символов char
+//str = "привет" строка по сути это массив символов(char)
+//str[0] = 'п'(char)
+
+                                                                           // ЗАДАЧА 1
+
+//Задайте массив символов (тип char []). 
+// Создайте строку из символов этого массива. 
+
+//[‘a’, ‘b’, ‘c’, ‘d’] => “abcd” 
+
+// string GetStringFromCharArray (char[] array) // ФУНКЦИЯ КОТОРАЯ БЕРЕТ ТИП ДАННЫХ char и возводит в string 
+// {
+//     // array = [‘a’, ‘b’, ‘c’, ‘d’] 
+//     string result = string.Empty; // ""   // Empty создает пустую строчку; 
+//     foreach (char symbol in array) // цикл foreach проходит все символы массива char и закидывает их в переменную symbol
+//     // symbol = 'a', symbol = 'b', symbol = 'c', symbol = 'd'
+//     {
+//         result += symbol;                   // result = result + symbol //изначально result был пустым и потом += symbol туда зашли все символы 
+//         // result = "" + "a"; result = "a"
+//         // result = "a" +"b"; result = "ab"
+//         // result = "ab" + "c"; result = "abc"
+//         // result = "abc" + "d"; result = "abcd"
+//     }
+//     return result; // после прохода циклом foreach обновим переменную result; ч 
+// }
+// // '1' - char,символ одинарные кавычки  "3rr2" - string строка двойные кавычки
+// char[] chars = {'1', 'd', '!', '2', 'f'}; // массив с символами 
+// //Console.WriteLine($"Массив: [{string.Join("; ", chars)}]"); 
+// string res = GetStringFromCharArray(chars);// массив склеиваем в одну большую строчку и потом эту строчку сохраняем в res // ФУНКЦИЯ КОТОРАЯ ИСПОЛЬЗУЕТ МАССИВ ДАСТ СТРОКУ res
+// Console.WriteLine(res); //показать строку res
+
+                                                                             // ЗАДАЧА 2
+
+// На основе символов строки (тип string) сформировать массив // ЗАДАЧА НАОБОРОТ 
+// символов (тип char[]). Вывести массив на экран.
+
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
+
+char[] ConvertStringToCharArray (string str)           // ПИШЕМ char[] Т.К. ПОСЛЕ ФУНКЦИИ ХОТИМ ПОЛУЧИТЬ МАССИВ А ИСПОЛЬЗОВАТЬ БУДЕМ СТРОКУ  str
+{
+    char[] chars = new char[str.Length]; // Создаем массив chars и выделяем ему место [str.Length] столько элементов массива сколько букв в строке string str
+    // "hi" => [,], str.Length = 2, массив на 2 элемента
+    for (int i = 0; i < str.Length; i++)
+    {
+        chars[i] = str[i]; // 1й элемент массива равен пермому символу строки
+        //chars[0] = str[0], chars[0]=h
+        //chars[1] = str[1], chars[1]=i
+        // chars = ['h', 'i']
+    }
+    return chars;  // возвращаем заполненый массив chars строкой str
+    // str = "hi" => [h,i]
+    // "hi" => [str[0], str[1]]
+}
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
+string str = "Hello world";                     // НАША СТРОКА КОТОРУЮ ФУНКЦИЯ ConvertStringToCharArray БУДЕТ ВОЗВОДИТЬ В МАССИВ
+Console.WriteLine($"Строчка исходная: {str}");  // ВЫВОД СТРОКИ 
+char[] chars = ConvertStringToCharArray(str);   // ВЫЗОВ ФУНКЦИИ ОНА БЕРЕТ СТРОКУ str И ОТДАЕТ МАССИВ chars
+Console.WriteLine($"Массив: [{string.Join("; ", chars)}]"); // ВЫВОД МАССИВЫ 
+
+
+                                                                             // ЗАДАЧА 3
